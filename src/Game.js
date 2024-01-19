cf3c29a05e0e9bdc64ba42d9720b05b5b37ea136
threeBearsWithBike <trois@list.ru>
2024-01-19 14:53:01 +0300

    add remove lines, level and score

--- src/Game.js
+++ src/Game.js
@@ -1,10 +1,19 @@
 export default class Game {
+    static points = {
+        '1': 40,
+        '2': 100,
+        '3': 300,
+        '4': 1200
+    }
     score = 0;
     lines = 0;
-    level = 0;
     playField = this.createPlayField();
     activePiece = this.createPiece();
     nextPiece = this.createPiece();
+
+    get level() {
+        return Math.floor(this.lines * 0.1);
+    }
 
     getState() {
         const playField = this.createPlayField();
@@ -134,6 +143,8 @@ export default class Game {
         if (this.hasCollision()) {
             this.activePiece.y -= 1;
             this.lockPiece();
+            const clearedLines = this.clearLines();
+            this.updateScore(clearedLines);
             this.updatePieces();
         }
     }
@@ -197,6 +208,44 @@ export default class Game {
                         this.playField[pieceY + y][pieceX + x] = blocks[y][x];
                     }
                 }
+        }
+    }
+
+    clearLines() {
+        const rows = 20;
+        const columns = 10;
+        let lines = [];
+
+        for (let y = rows - 1; y >= 0; y--) {
+            let numberOfBlocks = 0;
+
+            for (let x = 0; x < columns; x++) {
+                if (this.playField[y][x]) {
+                    numberOfBlocks += 1;
+                }
+            }
+
+            if (numberOfBlocks == 0) {
+                break;
+            } else if (numberOfBlocks < columns) {
+                continue;
+            } else if (numberOfBlocks === columns) {
+                lines.unshift(y);
+            }
+        }
+
+        for (let index of lines) {
+            this.playField.splice(index, 1);
+            this.playField.unshift(new Array(columns).fill(0));
+        }
+
+        return lines.length;
+    }
+
+    updateScore(clearedLines) {
+        if (clearedLines > 0) {
+            this.score += Game.points[clearedLines] * (this.level + 1);
+            this.lines = clearedLines;
         }
     }
 
